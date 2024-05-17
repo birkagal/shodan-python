ab18a18a0d445a178e402a6fea9146d0f4c5d1f2
Gal Birka <birkagal@gmail.com>
2024-05-17 09:07:39 +0300

    optional status_code in APIError

--- shodan/exception.py
+++ shodan/exception.py
@@ -1,7 +1,8 @@
 class APIError(Exception):
     """This exception gets raised whenever a non-200 status code was returned by the Shodan API."""
-    def __init__(self, value):
+    def __init__(self, value, status_code=None):
         self.value = value
+        self.status_code = status_code
 
     def __str__(self):
         return self.value
